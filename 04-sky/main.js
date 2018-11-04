27573a9568d21475168a414a11d959a9b8d29add
Unicar <unicar9@gmail.com>
2018-11-04 15:09:23 +1100

    Added day 4

--- 04-sky/main.js
+++ 04-sky/main.js
@@ -1,31 +1,57 @@
-var world = document.getElementById( 'sky' ),
-  viewport = document.getElementById( 'viewport' ),
-  worldXAngle = 0,
-  worldYAngle = 0,
-  d = 0;
+let sky = document.getElementById('sky'),
+  viewport = document.getElementById('viewport'),
+  skyXAngle = 0,
+  skyYAngle = 0,
+  d = 0
+  clouds = []
 
-/*
-  Event listener to transform mouse position into angles
-  from -180 to 180 degress, both vertically and horizontally
-*/
-window.addEventListener( 'mousemove', function( e ) {
+window.addEventListener('mousemove', e => {
+  skyYAngle = -( .5 - ( e.clientX / window.innerWidth ) ) * 90
+  skyXAngle = ( .5 - ( e.clientY / window.innerHeight ) ) * 90
+  updateView()
+})
 
-  worldYAngle = -( .5 - ( e.clientX / window.innerWidth ) ) * 180;
-  worldXAngle = ( .5 - ( e.clientY / window.innerHeight ) ) * 180;
-  updateView();
-
-} );
-
-/*
-  Changes the transform property of world to be
-  translated in the Z axis by d pixels,
-  rotated in the X axis by worldXAngle degrees and
-  rotated in the Y axis by worldYAngle degrees.
-*/
 function updateView() {
-
-  world.style.transform = `translateZ(${d}px) \
-  rotateX(${worldXAngle}deg) \
-  rotateY(${worldYAngle}deg)`;
-
+  sky.style.transform = `translateZ(${d}px) \
+  rotateX(${skyXAngle}deg) \
+  rotateY(${skyYAngle}deg)`
 }
+
+function generate() {
+    clouds = []
+
+    while (sky.firstChild) {
+        sky.removeChild(sky.firstChild)
+    }
+  
+    new Array(20).fill(1).forEach(_ => clouds.push( createCloud() ))  
+}
+
+function randy(min, max) {
+   return Math.floor(min + (max - min) * Math.random())
+}
+
+function createCloud() {
+
+    const div = document.createElement('div')
+    div.className = 'cloud'
+    const t = `translateX(${randy(0, 0.9 * window.innerWidth)}px) \
+    translateY(${randy(0, 0.9 * window.innerHeight)}px) \
+    translateZ(${randy(50,200)}px) \
+    scale(${randy(3, 6) * 0.1})`
+    div.style.transform = t
+
+    // const animation = `move ${randy(5, 15)} linear infinite;`
+    // div.style.animation = animation
+    div.animate({ 
+        marginLeft: ['0px', `${randy(50, 100)}px`, '0px'] 
+    },{ 
+        duration: randy(5000, 15000),
+        iterations: Infinity
+    })
+    sky.appendChild( div )
+  
+    return div
+}
+  
+generate()
